7997f00573c3e8ebdee6483d0c5d83064ecdf658
DWsatrya <dewanyomansatrya16@gmail.com>
2026-06-26 15:38:54 +0700

    membuat tugas hari 3

--- tempat script/GameManager.gd
+++ tempat script/GameManager.gd
@@ -1,18 +1,12 @@
 extends Node
 
 # Data global
-var score: int = 0
 var coins: int = 0
 var lives: int = 3
 
 # Signal agar UI bisa update otomatis
-signal score_changed(new_score)
 signal coins_changed(new_coins)
 signal lives_changed(new_lives)
-
-func add_score(amount: int) -> void:
-	score += amount
-	emit_signal("score_changed", score)
 
 func add_coin() -> void:
 	coins += 1
@@ -26,4 +20,3 @@ func lose_life() -> void:
 
 func game_over() -> void:
 	print("Game Over!")
-	# Nanti bisa ganti scene ke game over screen

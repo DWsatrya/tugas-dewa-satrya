ef07a6831fbe678ea8609fbf4d668b6577597958
DWsatrya <dewanyomansatrya16@gmail.com>
2026-06-26 12:21:52 +0700

    ui untuk menampilkan sisa nyawa serta jumlah coin yang di ambil

---
+++ UI.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	var label_lives = $VBoxContainer/LabelLives
+	var label_coins = $VBoxContainer/LabelCoins
+	
+	label_lives.text = "Nyawa: " + str(GameManager.lives)
+	label_coins.text = "Koin: " + str(GameManager.coins)
+	
+	GameManager.lives_changed.connect(func(val): label_lives.text = "Nyawa: " + str(val))
+	GameManager.coins_changed.connect(func(val): label_coins.text = "Koin: " + str(val))
+	
+	

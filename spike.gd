8ce3c3d4112621a4b6dc4c66e215b3c7f7eda8d5
DWsatrya <dewanyomansatrya16@gmail.com>
2026-06-29 10:28:21 +0700

    fix eror

--- spike.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		print("Sebelum panggil GameManager")
-		GameManager.lose_life()
-		print("Sesudah panggil GameManager")
